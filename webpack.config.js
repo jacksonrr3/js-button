caa949ffaf06532d760478a1b081ce913766f489
CORP\zaborshchikov.e <jacksonrr3@yandex.ru>
2021-03-26 13:35:00 +0300

    fix jest tests

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
-const path = require('path');
+// const path = require('path');
+// import path from 'path';
 
-module.exports = {
-     mode: "production", 
+// module.exports = {
+export default {
+    mode: "production", 
 
-     output: {
-          filename: "js-button.js"
-     }
+    output: {
+        filename: "js-button.js"
+    },
+     
+    resolve: {
+        extensions: ['.js', '.json'],
+        enforceExtension: true,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|test)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+    }
+        
 }
