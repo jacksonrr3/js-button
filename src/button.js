08cf95e4a8d99802752611a7b703800df7fcd7d7
CORP\zaborshchikov.e <jacksonrr3@yandex.ru>
2021-03-31 18:14:02 +0300

    Add class selector at node in button constructor.

--- src/button.js
+++ src/button.js
@@ -2,16 +2,16 @@ class Button {
     constructor(node, options){
         this.node = node;
         this.node.innerText = options.text;
-        this.node.className = 'button';
+        this.node.classList.add('button');
         this.onClick = options.onClick;
         this.node.addEventListener('click', this.onClick);
     }
 
     option(options){
-        if (options.hasOwnProperty('text')){
+        if (options.hasOwnProperty('text')) {
             this.node.innerText = options.text;
         }
-        if (options.hasOwnProperty('onClick')){
+        if (options.hasOwnProperty('onClick')) {
             this.node.removeEventListener('click', this.onClick);
             this.onClick = options.onClick;
             this.node.addEventListener('click', this.onClick);
--- test/button.test.js
+++ test/button.test.js
@@ -1,46 +1,53 @@
 const Button = require('../src/button.js').Button;     
 
 test('Сhange node text', () => {
-    const testNode = document.createElement('div');
+    const buttonNode = document.createElement('div');
     const text = 'test_text';
-    const testObj = new Button(testNode, {text: text});
-    expect(testNode.innerText).toBe(text);
+    const testObj = new Button(buttonNode, {text: text});
+    expect(buttonNode.innerText).toBe(text);
 });
 
 test('Add class button', () => {
-    const testNode = document.createElement('div');
+    const buttonNode = document.createElement('div');
     const className = 'button';
-    const testObj = new Button(testNode, {text: ''});
-    expect(testNode.className).toBe(className);
+    const testObj = new Button(buttonNode, {text: ''});
+    expect(buttonNode.className).toBe(className);
 });
 
 test('Add eventHandler', () => {
-    const testNode = document.createElement('div');
+    const buttonNode = document.createElement('div');
     let handlerIsCalled;
     const clickHandler = function() {
         handlerIsCalled = true;
     };
-    const testObj = new Button(testNode, {text: '', onClick: clickHandler});
-    testNode.dispatchEvent(new Event('click'));
+    const testObj = new Button(buttonNode, {text: '', onClick: clickHandler});
+    buttonNode.dispatchEvent(new Event('click'));
     expect(handlerIsCalled).toBe(true);
 });
 
 test('Change text option', () => {
-    const testNode = document.createElement('div');
+    const buttonNode = document.createElement('div');
     const text = 'test_text';
-    const testObj = new Button(testNode, {});
+    const testObj = new Button(buttonNode, {});
     testObj.option({text: text});
-    expect(testNode.innerText).toBe(text);
+    expect(buttonNode.innerText).toBe(text);
 });
 
 test('Change onClick option', () => {
-    const testNode = document.createElement('div');
+    const buttonNode = document.createElement('div');
     let handlerIsCalled = false;
     const clickHandler = function() {
         handlerIsCalled = true;
     };
-    const testObj = new Button(testNode, {});
+    const testObj = new Button(buttonNode, {});
     testObj.option({onClick: clickHandler});
-    testNode.dispatchEvent(new Event('click'));
+    buttonNode.dispatchEvent(new Event('click'));
     expect(handlerIsCalled).toBe(true);
+});
+
+test('Add class selector at node', () => {
+    const buttonNode = document.createElement('div');
+    buttonNode.className = 'element';
+    const buttonObject = new Button(buttonNode, {});
+    expect(buttonNode.className).toBe('element button');
 });
