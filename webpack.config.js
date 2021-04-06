d6498ff1671a9ee5671b08bb64882bedc702946f
CORP\zaborshchikov.e <jacksonrr3@yandex.ru>
2021-04-06 17:40:25 +0300

    added ES6 modules support

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,25 @@
-// const path = require('path');
-// import path from 'path';
-
-// module.exports = {
 export default {
-    mode: "production", 
+  mode: "production", 
 
-    output: {
-        filename: "js-button.js"
-    },
-     
-    resolve: {
-        extensions: ['.js', '.json'],
-        enforceExtension: true,
-    },
-    module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|test)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
+  output: {
+      filename: "js-button.js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        resolve: {
+          fullySpecified: false
+        },
+        exclude: /(node_modules|test|dist)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
           }
-        ]
-    }
-        
+        }
+      }
+    ]
+  }
 }
